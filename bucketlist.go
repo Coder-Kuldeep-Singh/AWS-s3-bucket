98b5c30790a0c51de6ef6c782225b9282b33ba22
mavensingh <kuldeep.avsar@gmail.com>
2020-05-27 14:06:31 +0530

    printing all the list of buckets you have created in AWS s3

---
+++ bucketlist.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+
+	s, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("REGION")),
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("SECRETID"),  // id
+			os.Getenv("SECRETKEY"), // secret
+			""), // token can be left blank for now
+	})
+
+	// Create S3 service client
+	svc := s3.New(s)
+
+	result, err := svc.ListBuckets(nil)
+	if err != nil {
+		exitErrorf("Unable to list buckets, %v", err)
+	}
+
+	fmt.Println("Buckets:")
+
+	for _, b := range result.Buckets {
+		fmt.Printf("* %s created on %s\n",
+			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	}
+
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
